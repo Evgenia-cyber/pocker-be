553a6dc2b41d8eabb05997a3a0ecf59d95567e10
Evgenia Kurineva <kurineva85@mail.ru>
2021-09-05 15:19:26 +0300

    refactor: move default port value into constants.js

--- common/config.js
+++ common/config.js
@@ -1,11 +1,12 @@
 const dotenv = require('dotenv');
 const path = require('path');
+const DEFAULT_PORT = require('./constants');
 
 dotenv.config({
   path: path.join(__dirname, '../.env'),
 });
 
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || DEFAULT_PORT,
   MONGO_URL: process.env.MONGO_URL,
 };
